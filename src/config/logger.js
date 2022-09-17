12b82e7d99ed46b18b16cea0916a7fb316985b42
Benjamin <benver17@gmail.com>
2022-09-17 17:46:27 -0500

    updated logger.js uses prettyPrint in development

--- src/config/logger.js
+++ src/config/logger.js
@@ -3,9 +3,14 @@ const { nanoid } = require("nanoid");
 
 const level = process.env.LOG_LEVEL || "info"
 
+const nodeEnv = process.env.NODE_ENV || 'development'
+const prettyPrint = nodeEnv === "development"
+
+
 const logger = pinoHttp({
   genReqId: (request) => request.headers['x-request-id'] || nanoid(),
-  level
+  level,
+  prettyPrint
 });
 
 module.exports = logger;
